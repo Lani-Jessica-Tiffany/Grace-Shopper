020723ddcee69d697aec4aa1be650c0dd4164fbb
jchoi37 <j@users-MacBook.local>
2020-04-21 15:14:39 -0400

    merge seed file with original seed, updated routes to remove the admin only

--- script/seed.js
+++ script/seed.js
@@ -2,17 +2,63 @@
 
 const db = require('../server/db')
 const {User} = require('../server/db/models')
+const {Boba} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@email.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'janedoe@email.com',
+      password: 'abc'
+    }),
+    User.create({
+      firstName: 'Sally',
+      lastName: 'Mae',
+      email: 'sallymae@email.com',
+      isAdmin: true,
+      password: 'abc'
+    })
+  ])
+
+  const allBoba = await Promise.all([
+    Boba.create({
+      name: 'Classic Milk Tea',
+      price: 4.5,
+      description: 'Black tea blend with milk',
+      imageUrl:
+        'https://raster-static.postmates.com/?url=com.postmates.img.prod.s3.amazonaws.com%2F6c08ded2-e631-406d-8233-282237558765%2Forig.jpg&quality=85&w=500&h=0&mode=auto&format=webp&v=4',
+      quantity: 1
+    }),
+    Boba.create({
+      name: 'Thai Tea',
+      price: 4.75,
+      description: 'Sweet, creamy & rich, with organic condensed milk',
+      imageUrl:
+        'https://theforkedspoon.com/wp-content/uploads/2018/06/Thai-Tea-Recipe-5-700x1050.jpg',
+      quantity: 3
+    }),
+    Boba.create({
+      name: 'Matcha Bubble Tea',
+      price: 5.75,
+      description: 'Hand-whisked organic Japanese green tea',
+      imageUrl:
+        'https://www.ohhowcivilized.com/wp-content/uploads/2019/05/0519-matcha-bubble-tea-9.jpg',
+      quantity: 2
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${allBoba.length} boba`)
   console.log(`seeded successfully`)
 }
 
--- server/api/boba.js
+++ server/api/boba.js
@@ -5,84 +5,67 @@ module.exports = router
 
 // do we need to add and http error middleware?
 
-//find user
-router.params('id', async (req, res, next id) => {
-  try {
-      const checkUser = await User.findByPk(id)
-      if(!checkUser) {
-        res.status(400)
-        } else {
-          req.requestedUser = checkUser
-          next()
-          return null
-       }
-    } catch (err) {
-      next(err)
-    }
-  })
-
 // find admin users
 const adminOnly = (req, res, next) => {
-  if(!req.session.user.isAdmin){
-    const err = new Error("Not Allowed")
-    err.status= 401
+  if (!req.sessions.user.isAdmin) {
+    const err = new Error('Not Allowed')
+    err.status = 401
     return next(err)
   }
   next()
 }
 
-//Add Boba
-router.post('/', adminOnly, async (req, res, next) => {
-  try{
+//Add Boba -ADMIN FUNCTION
+router.post('/', async (req, res, next) => {
+  try {
     const newBoba = await Boba.create(req.body)
-    const response =
-      { message: 'Boba Added!',
-        boba: newBoba.datavalues }
+    const response = {
+      message: 'Boba Added!',
+      boba: newBoba.datavalues
+    }
     res.json(response)
-  } catch (err){
+  } catch (err) {
     next(err)
   }
 })
 
-//Update Boba
-router.put('/:id', adminOnly, async (req, res, next) => {
+//Update Boba -ADMIN FUNCTION
+router.put('/:id', async (req, res, next) => {
   try {
-    const updated = await Boba.update(req.body, { where: {id: req.params.id}})
+    const updated = await Boba.update(req.body, {where: {id: req.params.id}})
     res.json(updated)
   } catch (err) {
-    next (err)
+    next(err)
   }
 })
 
-//Delete Boba
-router.destroy('/:id', adminOnly, async (req, res, next) => {
+//Delete Boba -ADMIN FUNCTION
+router.delete('/:id', async (req, res, next) => {
   try {
     const deleteBoba = await Boba.findByPk(req.params.id)
     Boba.destroy(deleteBoba)
     res.status(204).end()
-  } catch (err){
+  } catch (err) {
     next(err)
   }
 })
 
 // Single View Boba
 router.get('/:id', async (req, res, next) => {
-  try{
+  try {
     const oneBoba = await Boba.findByPk(req.params.id)
     res.json(oneBoba)
-  } catch (err){
+  } catch (err) {
     next(err)
   }
 })
 
 //All Boba
 router.get('/', async (req, res, next) => {
-  try{
+  try {
     const allBoba = await Boba.findAll()
     res.json(allBoba)
-  } catch (err){
+  } catch (err) {
     next(err)
   }
 })
-
-
