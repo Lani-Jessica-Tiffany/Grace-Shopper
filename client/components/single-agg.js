67a7e66f32539b47a4869080825cb1b5e43598fd
tiff <wong.tiffanyh@gmail.com>
2020-04-21 14:43:37 -0400

    f(mainpage): add to cart component WIP

--- client/components/single-agg.js
+++ client/components/single-agg.js
@@ -3,11 +3,11 @@ import React from 'react'
 import {SingleBasic, SingleDetail} from './single-info'
 
 // component
-const SingleAgg = ({boba}) => (
+const SingleAgg = ({boba, boba: {id}}) => (
   <div>
     {/* aggregate all components for boba here (e.g. info, quantity, add-ons) */}
-    <SingleBasic key={`sb${boba.id}`} boba={boba} />
-    <SingleDetail key={`sd${boba.id}`} boba={boba} />
+    <SingleBasic key={`sb${id}`} boba={boba} />
+    <SingleDetail key={`sd${id}`} boba={boba} />
   </div>
 )
 
--- client/store/boba.js
+++ client/store/boba.js
@@ -1,6 +1,7 @@
 // action type
 const GET_ALL = 'GET_ALL'
 const GET_SINGLE = 'GET_SINGLE'
+const UPDATE = 'UPDATE'
 
 // action creator
 const getAll = all => ({
@@ -11,6 +12,11 @@ const getAll = all => ({
 const getSingle = single => ({
   type: GET_SINGLE,
   single
+})
+
+const update = quantity => ({
+  type: UPDATE,
+  quantity
 })
 
 // thunk creator
@@ -56,20 +62,45 @@ export const getSingleThunk = id => async (dispatch, getState, {axios}) => {
     console.log(err)
   }
 }
+export const updateThunk = (id, quantity) => async (
+  dispatch,
+  getState,
+  {axios}
+) => {
+  try {
+    //  const {data} = await axios.put(`/api/campuses/${campusId}`, input)
+    const data = {
+      id: 1,
+      name: 'oolong',
+      price: 5,
+      description: 'best boba ever',
+      image: 'https://media.giphy.com/media/kBHfPMfonjJmM9fpfx/giphy.gif',
+      // new field
+      quantity: 0
+    }
+    const action = update(data)
+    dispatch(action)
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 // state
 const initialState = {
   all: [],
-  single: {}
+  single: {},
+  update: 0
 }
 
 // reducer
 const boba = (state = initialState, action) => {
   switch (action.type) {
     case GET_ALL:
-      return {...initialState, all: action.all}
+      return {...state, all: action.all}
     case GET_SINGLE:
-      return {...initialState, single: action.single}
+      return {...state, single: action.single}
+    case UPDATE:
+      return {...state, update: action.quantity}
     default:
       return state
   }
