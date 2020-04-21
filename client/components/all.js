aa264328df8827b71478e0388d54b15976da81c4
tiff <wong.tiffanyh@gmail.com>
2020-04-21 09:05:34 -0400

    f(mainpage): update All from functional to class component WIP

--- client/components/all.js
+++ client/components/all.js
@@ -1,16 +1,39 @@
 // import
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+// import { getAllCampusesFromServer } from '../redux/campuses';
 import SingleAgg from './single-agg'
 
 // component
-const All = () => {
-  const boba = {
+class All extends Component {
+  // componentDidMount() {
+  //   this.props.getAllCampusesFromStore();
+  // }
+  render() {
+    return <SingleAgg {...this.props} />
+  }
+}
+
+// connect
+const mapStatetoProps = state => ({
+  boba: {
     name: 'oolong',
     price: 5,
     description: 'best boba ever',
     image: 'https://media.giphy.com/media/kBHfPMfonjJmM9fpfx/giphy.gif'
   }
-  return <SingleAgg boba={boba} />
-}
+})
+
 // export
-export default All
+export default connect(mapStatetoProps)(All)
+// export default All
+
+// const mapStatetoProps = state => ({
+//   campuses: state.campuses.all,
+// });
+
+// const mapDispatchtoProps = dispatch => ({
+//   getAllCampusesFromStore: () => dispatch(getAllCampusesFromServer()),
+// });
+
+// export default connect(mapStatetoProps, mapDispatchtoProps)(AllCampuses);
