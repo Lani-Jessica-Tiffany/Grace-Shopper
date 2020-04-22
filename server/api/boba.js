5e9ba7e23ff0b570547151f11277b82b0b9216b3
jchoi37 <j@users-MacBook.local>
2020-04-22 09:14:04 -0400

    added admin only for boba routes, created working user routes, NOT yet admin protected

--- server/api/boba.js
+++ server/api/boba.js
@@ -3,11 +3,9 @@ const {User} = require('../db/models')
 const {Boba} = require('../db/models')
 module.exports = router
 
-// do we need to add and http error middleware?
-
-// find admin users
+// check if admin
 const adminOnly = (req, res, next) => {
-  if (!req.sessions.user.isAdmin) {
+  if (!req.user.isAdmin) {
     const err = new Error('Not Allowed')
     err.status = 401
     return next(err)
@@ -16,7 +14,7 @@ const adminOnly = (req, res, next) => {
 }
 
 //Add Boba -ADMIN FUNCTION
-router.post('/', async (req, res, next) => {
+router.post('/', adminOnly, async (req, res, next) => {
   try {
     const newBoba = await Boba.create(req.body)
     const response = {
@@ -30,9 +28,11 @@ router.post('/', async (req, res, next) => {
 })
 
 //Update Boba -ADMIN FUNCTION
-router.put('/:id', async (req, res, next) => {
+router.put('/:id', adminOnly, async (req, res, next) => {
   try {
-    const updated = await Boba.update(req.body, {where: {id: req.params.id}})
+    const updated = await Boba.update(req.body, {
+      where: {id: req.params.id}
+    })
     res.json(updated)
   } catch (err) {
     next(err)
@@ -40,10 +40,11 @@ router.put('/:id', async (req, res, next) => {
 })
 
 //Delete Boba -ADMIN FUNCTION
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', adminOnly, async (req, res, next) => {
   try {
-    const deleteBoba = await Boba.findByPk(req.params.id)
-    Boba.destroy(deleteBoba)
+    await Boba.destroy({
+      where: {id: req.params.id}
+    })
     res.status(204).end()
   } catch (err) {
     next(err)
--- server/api/users.js
+++ server/api/users.js
@@ -2,7 +2,47 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+//CHECKING ADMIN IS NOT WORKING YET
+//check the user
+
+//Single user - individual user and admin
+router.get('/:id', async (req, res, next) => {
+  try {
+    const oneUser = await User.findByPk(req.params.id)
+    res.json(oneUser)
+  } catch (err) {
+    next(err)
+  }
+})
+// Add User -ADMIN Function (this might be problem to tackle later)
+
+//Update User - individual user and admin
+router.put('/:id', async (req, res, next) => {
+  try {
+    const updated = await User.update(req.body, {
+      where: {id: req.params.id}
+    })
+    res.json(updated)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Delete - individual user and admin
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await User.destroy({
+      where: {id: req.params.id}
+    })
+    res.status(204).end()
+  } catch (err) {
+    next(err)
+  }
+})
+
+// All users -ADMIN FUNCTION
+router.get('/', adminOnly, async (req, res, next) => {
+  console.log(req.user, 'USER')
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
