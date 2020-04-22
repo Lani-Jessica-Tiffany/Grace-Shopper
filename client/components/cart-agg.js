da09c26cfa380e2041e6829ebb1fdaa3af35baf7
tiff <wong.tiffanyh@gmail.com>
2020-04-22 19:01:10 -0400

    add 2 decimal places to price

--- client/components/cart-agg.js
+++ client/components/cart-agg.js
@@ -1,12 +1,12 @@
 // import
 import React from 'react'
-import {SingleBasic} from './single-info'
+import {SingleCart} from './single-info'
 
 // component
 const CartAgg = ({bobas}) => (
   <div>
     {/* aggregate all components for boba here (e.g. info, quantity, add-ons) */}
-    {bobas.map(boba => <SingleBasic key={`boba${boba.id}`} boba={boba} />)}
+    {bobas.map(boba => <SingleCart key={`boba${boba.id}`} boba={boba} />)}
   </div>
 )
 
