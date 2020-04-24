eab886fea3028d3cade5849a2337f6f6782f7df9
tiff <wong.tiffanyh@gmail.com>
2020-04-24 13:40:55 -0400

    update price format

--- client/components/single-info.js
+++ client/components/single-info.js
@@ -12,7 +12,7 @@ export const SingleBasic = ({boba: {id, name, imageUrl}}) => (
 )
 
 export const SingleDetail = ({boba: {price, description}}) => {
-  price = '' + price
+  price = String(price)
   price = price.slice(0, price.length - 2) + '.' + price.slice(price.length - 2)
   return (
     <div>
@@ -25,12 +25,14 @@ export const SingleDetail = ({boba: {price, description}}) => {
 export const SingleCart = ({
   boba: {bobaId, name, imageUrl, price, quantity}
 }) => {
+  price = String(price)
+  price = price.slice(0, price.length - 2) + '.' + price.slice(price.length - 2)
   return (
     <div>
       <Link to={`/boba/${bobaId}`}>{name}</Link>
       <br />
       <img src={imageUrl} className="bobaImg" />
-      <h5>Price: ${(price / 100).toFixed(2)}</h5>
+      <h5>Price: ${price}</h5>
       <h5>Quantity: {quantity}</h5>
     </div>
   )
