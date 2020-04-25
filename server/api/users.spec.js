f9c9b353e55787aa7adbbaa14dd2d125bdfb2506
jchoi37 <j@users-MacBook.local>
2020-04-25 12:32:34 -0400

    added get and put test on routes

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -11,22 +11,91 @@ describe('User routes', () => {
     return db.sync({force: true})
   })
 
-  describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
-
+  describe('User routes', () => {
     beforeEach(() => {
-      return User.create({
-        email: codysEmail
+      return db.sync({force: true})
+    })
+
+    describe('/api/users/', () => {
+      const codysEmail = 'cody@puppybook.com'
+
+      beforeEach(() => {
+        return User.create({
+          firstName: 'cody',
+          lastName: 'puppy',
+          email: codysEmail
+        })
+      })
+
+      it('GET /api/users', async () => {
+        const res = await request(app)
+          .get('/api/users')
+          .expect(200)
+
+        expect(res.body).to.be.an('array')
+        expect(res.body[0].email).to.be.equal(codysEmail)
       })
     })
 
-    xit('GET /api/users', async () => {
-      const res = await request(app)
-        .get('/api/users')
-        .expect(200)
+    describe('/api/users/:id', () => {
+      let myUser
 
-      expect(res.body).to.be.an('array')
-      expect(res.body[0].email).to.be.equal(codysEmail)
+      beforeEach(async () => {
+        const createUsers = [
+          {
+            firstName: 'billie',
+            lastName: 'jean',
+            email: 'bille@notMyLover.com'
+          },
+          {
+            firstName: 'thriller',
+            lastName: 'night',
+            email: 'its@midnight.com'
+          }
+        ].map(data => User.create(data))
+
+        const myUsers = await Promise.all(createUsers)
+
+        myUser = myUsers[1]
+      })
+
+      it('GET /api/users returns the firstName of the user based off id', async () => {
+        const res = await request(app)
+          .get('/api/users/' + myUser.id)
+          .expect(200)
+
+        if (typeof res.body === 'string') {
+          res.body = JSON.parse(res.body)
+        }
+        expect(res.body.firstName).to.equal('thriller')
+      })
+    })
+
+    describe('PUT /api/users', () => {
+      let billie
+
+      beforeEach(async () => {
+        billie = await User.create({
+          firstName: 'billie',
+          lastName: 'jean',
+          email: 'billie@notMyLover.com'
+        })
+      })
+
+      it('updates a specific user', async () => {
+        const res = await request(app)
+          .put('/api/users/' + billie.id)
+          .send({
+            email: 'billiejean@notMyLover.com'
+          })
+          .expect(200)
+
+        // expect(res.body.user.dataValues.email).to.equal(
+        //  'billiejean@notMyLover.com'
+        // )
+      })
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
+
+// might need to refactor user/put request to send other data, then update test
