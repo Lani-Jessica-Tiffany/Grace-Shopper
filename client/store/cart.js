0fc56fc9d07e080a94976a7ec721a166e37765c7
Lani Tran <lanitran7@gmail.com>
2020-04-27 01:40:04 -0400

    add action/thunk/reducer for adding orders and updating orders in cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,12 +1,24 @@
 // action type
 const GET_ALL = 'GET_ALL'
-
+const ADD_ORDER = 'ADD_ORDER'
+const UPDATE_ORDER = 'UPDATE_ORDER'
 const REMOVE_ORDER = 'REMOVE_ORDER'
 
 // action creator
 const getAll = all => ({
   type: GET_ALL,
   all
+})
+
+const addOrder = order => ({
+  type: ADD_ORDER,
+  order
+})
+
+const updateOrder = (order, quantity) => ({
+  type: UPDATE_ORDER,
+  order,
+  quantity
 })
 
 const removeOrder = order => ({
@@ -25,13 +37,39 @@ export const getAllThunk = () => async (dispatch, getState, {axios}) => {
   }
 }
 
-export const removeOrderThunk = orderId => async (
+export const addOrderThunk = (orderId, bobaId) => async (
   dispatch,
   getState,
   {axios}
 ) => {
   try {
-    const {data} = await axios.delete(`/api/cart/${orderId}`)
+    const {data} = await axios.post('/api/cart', {orderId, bobaId})
+    dispatch(addOrder(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const updateOrderThunk = (orderId, bobaId, quantity) => async (
+  dispatch,
+  getState,
+  {axios}
+) => {
+  try {
+    const {data} = await axios.put('/api/cart', {orderId, bobaId, quantity})
+    dispatch(updateOrder(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const removeOrderThunk = (orderId, bobaId) => async (
+  dispatch,
+  getState,
+  {axios}
+) => {
+  try {
+    const {data} = await axios.delete('/api/cart', {orderId, bobaId})
     dispatch(removeOrder(data))
   } catch (err) {
     console.log(err)
@@ -48,8 +86,26 @@ const cart = (state = initialState, action) => {
   switch (action.type) {
     case GET_ALL:
       return {...state, all: action.all}
+
+    case ADD_ORDER:
+      return {...state, all: [...state.all, action.all]}
+
+    case UPDATE_ORDER:
+      const newState = all.map(order => {
+        if (order.id === action.id) {
+          return {
+            ...order,
+            quantity: action.quantity
+          }
+        }
+      })
+      return {...state, all: newState}
+
     case REMOVE_ORDER:
-      return state.all.filter(boba => boba.id !== action.boba)
+      return {
+        ...state,
+        all: state.all.filter(order => order.id !== action.order)
+      }
     default:
       return state
   }
